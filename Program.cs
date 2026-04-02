b015c79cda93c2f8bcc6afff915e629b2ef7b786
Nathaline Santos <nathalinep@gmail.com>
2026-04-02 16:33:07 -0300

    Infos

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Programa para calcular o Índice de Massa Corporal (IMC) e classificar o resultado
+//Autor: Nathaline Santos
+//Data: 02/04/2026
+//Descrição: Este programa solicita ao usuário seu nome, idade, peso e altura, calcula o IMC e classifica o resultado de acordo com as categorias estabelecidas pela Organização Mundial da Saúde (OMS).
+//Observação: Certifique-se de que o usuário insira valores válidos para peso e altura para evitar erros de cálculo.
+//Nota: Este programa é apenas para fins educacionais e não deve ser usado como uma ferramenta de diagnóstico médico. Para avaliações de saúde, consulte um profissional de saúde qualificado.
+
+
+Console.WriteLine("Hello, World!");
 
 Console.Write("Digite seu nome: ");
 string nome = Console.ReadLine();
